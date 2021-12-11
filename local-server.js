fbfc1b011ac9166febd979a213735b6189117b98
ETWilliams10 <williamsevan10@gmail.com>
2021-12-10 20:57:15 -0500

    Revert "heroku deploy test"
    This reverts commit 845908f40dea96c4c2bc422b127aa01894a9fadc.

--- local-server.js
+++ local-server.js
@@ -1,46 +1,26 @@
-const express = require('express');
-const { engine } = require('express-handlebars');
-const session = require('express-session');
-const routes = require('./controllers');
-const path = require ('path');
-const dotenv = require('dotenv');
+// const express = require('express');
+// const { engine } = require('express-handlebars');
+// const session = require('express-session');
+// const routes = require('./controllers');
+// const path = require ('path');
+// const dotenv = require('dotenv');
 
-dotenv.config();
+// dotenv.config();
 
-// const sequelize = require('./config/connection');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const port = process.env.PORT || 3001;
 
-const app = express();
-const port = process.env.PORT || 3001;
+// const app = express();
 
-
-
-
-
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
-
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
-
-// app.use(session(sess));
+// app.engine('handlebars', engine());
+// app.set('view engine', 'handlebars');
+// app.set('views', './views');
 
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// app.use(routes);
 
-// sequelize.sync({ force: false }).then(() => {
-  app.listen(port, () => console.log('Now listening'));
-// });
+// app.listen(port, () => console.log(`Now listening at http://localhost:${port}`));
