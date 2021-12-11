212468c42d902119e2916127e2f8f87fb02b83cb
Todd Trulock <toddstrulock@gmail.com>
2021-12-10 20:03:18 -0600

    everything has been tested on the local server and debugged.

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const media = require('./media');
 
+const homeroutes = require ('./home-route')
+
+router.use('/', homeroutes)
+
 router.use('/api', apiRoutes);
 router.use('/media', media);
 
