845908f40dea96c4c2bc422b127aa01894a9fadc
ETWilliams10 <williamsevan10@gmail.com>
2021-12-10 20:51:40 -0500

    heroku deploy test

--- local-server.js
+++ local-server.js
@@ -1,26 +1,46 @@
-// const express = require('express');
-// const { engine } = require('express-handlebars');
-// const session = require('express-session');
-// const routes = require('./controllers');
-// const path = require ('path');
-// const dotenv = require('dotenv');
+const express = require('express');
+const { engine } = require('express-handlebars');
+const session = require('express-session');
+const routes = require('./controllers');
+const path = require ('path');
+const dotenv = require('dotenv');
 
-// dotenv.config();
+dotenv.config();
 
-// const port = process.env.PORT || 3001;
+// const sequelize = require('./config/connection');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const app = express();
-
-// app.engine('handlebars', engine());
-// app.set('view engine', 'handlebars');
-// app.set('views', './views');
+const app = express();
+const port = process.env.PORT || 3001;
 
 
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
 
-// app.listen(port, () => console.log(`Now listening at http://localhost:${port}`));
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
+
+// app.use(session(sess));
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+// sequelize.sync({ force: false }).then(() => {
+  app.listen(port, () => console.log('Now listening'));
+// });
