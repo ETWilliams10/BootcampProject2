90a7527830c39b998788475a7c28d88e356a5d5d
etalle <etalle5627@gmail.com>
2021-12-10 21:46:53 -0500

    lowercase p

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Gallery = require('./Gallery');
-const Painting = require('./Painting');
+const painting = require('./painting');
 
 Gallery.hasMany(Painting, {
   foreignKey: 'gallery_id',
